226ac70121d480ec7210d0c65a9ab03f3ee7ef64
baloi <baloi.galia@gmail.com>
2011-07-24 08:15:29 -0400

    Resident unique and present name validations tested

--- spec/models/resident_spec.rb
+++ spec/models/resident_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Resident do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def setUp
+    Resident.destroy_all
+  end
+  it "should have a name" do
+    name = 'Proco' 
+    resident = Resident.sample(:name => name)
+    resident.name.should == name
+  end
+
+  it "should have a unique name" do
+    name = 'Proco' 
+    r1 = Resident.create(:name => name)
+    r1.errors.count.should == 0
+    r2 = Resident.create(:name => name)
+    r2.errors.count.should == 1
+    residents = Resident.find(:all)
+    residents.count.should == 1
+  end
 end

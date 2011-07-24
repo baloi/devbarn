01c3ab10867254af26830094124a67c8a3ed7ea8
baloi <baloi.galia@gmail.com>
2011-07-24 11:54:44 -0400

    Resident unique name test done...

--- spec/models/resident_spec.rb
+++ spec/models/resident_spec.rb
@@ -14,9 +14,12 @@ describe Resident do
     name = 'Proco' 
     r1 = Resident.create(:name => name)
     r1.errors.count.should == 0
+    r1_id = r1.id
     r2 = Resident.create(:name => name)
     r2.errors.count.should == 1
     residents = Resident.find(:all)
     residents.count.should == 1
+    residents.first.id.should == r1_id 
+    residents.last.id.should == r1_id 
   end
 end

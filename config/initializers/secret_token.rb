62463e3826b2488b1bc062c44d08ebbfe6ec569b
baloi <baloi.galia@gmail.com>
2011-07-23 21:53:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devbarn::Application.config.secret_token = '3a1438f0474286c9d5ddff2a03263e0c0fd7502f4c84ea0fc8297f5f7d9bfe426ec9041e4a173663ddea646f7008ee28b9273b045406137ddfda7c48ac56c3da'
